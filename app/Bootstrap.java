c625b06f0ef1f3e60ac2b26b8f21adc62e5886f7
witmicko <mjogrodniczak@gmail.com>
2013-04-30 19:30:53 +0100

    added extra data.yml`s

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -10,7 +10,14 @@ public class Bootstrap extends Job
   @Override
   public void doJob() {
    
-      Fixtures.loadModels("data.yml");
+      //Fixtures.loadModels("data.yml");
+      Fixtures.loadModels("/data/data1.yml");
+      Fixtures.loadModels("/data/data2.yml");
+      Fixtures.loadModels("/data/data3.yml");
+      Fixtures.loadModels("/data/data4.yml");
+      Fixtures.loadModels("/data/data5.yml");
+      Fixtures.loadModels("/data/data6.yml");
+      Fixtures.loadModels("/data/data7.yml");
     
   }
 }
