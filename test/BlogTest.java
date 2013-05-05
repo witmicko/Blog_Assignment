5e399337a33521dc8f67e182d2cdb47bd8a7aca0
witmicko <mjogrodniczak@gmail.com>
2013-05-05 18:18:02 +0100

    fixed formatting in places user no longer can sub to his own posts from profile viewer

--- test/BlogTest.java
+++ test/BlogTest.java
@@ -7,69 +7,37 @@ import models.*;
 
 public class BlogTest extends UnitTest 
 {
-  private Blog b1,b2,b3;
-  private Post p1,p2,p3;
+  
+  
   @Before
   public void setup()
   {
-    p1 = new Post("title1", "content1", null);
-    p2 = new Post("title2", "content2", null);
-    p3 = new Post("title3", "content3", null);
-    b1 = new Blog("blog1");
-    b2 = new Blog("blog2");
-    b3 = new Blog("blog3");
     
-    b1.addPost(p1);
-    b1.addPost(p2);
-    b1.save();
-    b2.save();
-    b3.save();
   }
   
   @After
   public void teardown()
   {
-    b1.delete();
-    b2.delete();
-    b3.delete();
+   
   }
 
   @Test
   public void testCreate()
   {
-    Blog a = Blog.findByName("blog1");
-    assertNotNull(a);
-    assertEquals("blog1", a.name);
-    Blog b = Blog.findByName("blog2");
-    assertNotNull(b);
-    assertEquals("blog2", b.name);
-    Blog c = Blog.findByName("blog3");
-    assertNotNull(c);
-    assertEquals("blog3", c.name);
+    
   }
   @Test
   public void testNotThere()
   {
-    Blog a = Blog.findByName("blogNotThere");
-    assertNull(a);
+    
   }
   
   @Test
   public void testPosts(){
-    Blog blog = Blog.findByName("blog1");
-    assertEquals(2, blog.posts.size());
-    assertTrue(blog.posts.contains(p1));
-    assertTrue(blog.posts.contains(p2));
-    assertFalse(blog.posts.contains(p3));
+   
   }
   @Test
   public void testRemovePosts(){
-    Blog blog = Blog.findByName("blog1");
-    assertTrue(blog.posts.contains(p1));
-    blog.posts.remove(p1);
-    p1.delete();
-    blog.save();    
-    assertTrue(blog.posts.contains(p2));
-    assertFalse(blog.posts.contains(p3));  
+    
   }
 }
--- test/CommentTest.java
+++ test/CommentTest.java
@@ -1,7 +1,8 @@
 import org.junit.*;
 
-
 import java.util.*;
+
+import javax.crypto.AEADBadTagException;
 
 import play.test.*;
 import models.*;
@@ -9,47 +10,35 @@ import models.*;
 public class CommentTest extends UnitTest 
 {
   private User u1;
-  private Comment c1,c2,c3;
+  private Blog b1;
   private Post p1;
+  private Comment c1;
   @Before
   public void setup()
   {
-    u1= new User("bob", "lastname", 20, "bob@junit.com", "password");
-    c1 = new Comment("content1");
-   // c2 = new Comment("content2");
-    c3 = new Comment("content3");
-    p1 = new Post("title", "content", u1.firstName);
-  //  c2.save();
-    p1.addComment(c1);
-    p1.save();
-    c1.save();
-    u1.addComment(c3);
-    c3.save();
-    u1.save();    
+    c1 = new Comment("comment");
   }
   
   @After
   public void teardown()
   {
- //   c2.delete();
-    p1.comments.clear();
     c1.delete();
-    p1.delete();
-    u1.comments.clear();
-    c3.delete();
-    u1.delete();
   }
 
   @Test
   public void testCreate()
   {
-    assertEquals("content1", c1.content);
-    assertEquals("content1", c1.content);
-    assertEquals(p1, c2.post);
-    assertEquals(u1, c3.author);   
+    assertEquals("comment", c1.content);
+    Post p1 = new Post("title", "content", "user");
+    p1.addComment(c1);
+    assertEquals(c1.post,p1 );
+    assertTrue(p1.comments.contains(c1));
+    p1.removeComment(c1);
+    assertEquals(0, p1.comments.size());
   }
-  @Test
-  public void testPost(){
-    assertEquals(p1, c1.post);
-  }
+  
+  
+  
+  
+  
 }
